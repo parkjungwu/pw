7ea961759491501f4d896c5e19fbd2ad606697e4
ParkJeongWoo <pjw121599@naver.com>
2022-07-18 03:22:07 +0900

    initial

--- push.js
+++ push.js
@@ -9,6 +9,10 @@ let vapidkeys = {
 
 push.setVapidDetails('mailto:wjddn121599@gmail.com', vapidkeys.publicKey, vapidkeys.privateKey);
 
-let sub = {};
+let sub = {
+  endpoint:"https://fcm.googleapis.com/fcm/send/flhAFTILumM:APA91bFSAKnwbgRH0GI9crHAKjQGHViVX9H8wDPbnE8K4ZG54MxijFIx0uOp1nFa-8GNhAP9lmWeFUNkvYFtzgEwVvocTYnxWNPnrLrHXKUxUD6UvLxc6zs6P41NtVKdj0XU9ZQMhElH",
+  expirationTime:null,
+  keys:{p256dh:"BNrXZEDihtOERl4Q_uX16LgY7YhxJv4w6NjXo9NqdFH9129rctpNKuMZPcEzxtTnK74Yxjne8jqTMe8ivJdp4gY",
+  auth:"qFMCZjTltn_2mvMyPx-CZA"}}
 
 push.sendNotification(sub, 'test message')
