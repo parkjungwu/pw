a1c25dd6cd420a7e168dd5b3fbb263a34d569d66
ParkJeongWoo <pjw121599@naver.com>
2022-07-18 02:19:17 +0900

    initial

--- push.js
+++ push.js
@@ -1,5 +1,14 @@
 var push = require('web-push');
 
-let vapidkeys = push.generateVAPIDKeys();
+// let vapidkeys = push.generateVAPIDKeys();
+
+let vapidkeys = {
+    publicKey: 'BKVvfx2lz9q9F5RtmpCg9EGTskhLPl6BvA4nQ3yu7sag-NZKLDvP-x1gQSCLzWrpbYqnIHUOU05WhkaGuBfgR7Q',
+    privateKey: 'WZawaKoNsQtCEtsWoUS9ZNO6TfU4UE3ZCYslPWUsT10'
+  }
 
 push.setVapidDetails('mailto:wjddn121599@gmail.com', vapidkeys.publicKey, vapidkeys.privateKey);
+
+let sub = {};
+
+push.sendNotification(sub, 'test message')
