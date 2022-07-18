2d0126c80ff83716f2acacaa8576947f3abc67ce
ParkJeongWoo <pjw121599@naver.com>
2022-07-18 11:12:56 +0900

    initial

--- push.js
+++ push.js
@@ -10,9 +10,9 @@ let vapidkeys = {
 push.setVapidDetails('mailto:wjddn121599@gmail.com', vapidkeys.publicKey, vapidkeys.privateKey);
 
 let sub = {
-  endpoint:"https://fcm.googleapis.com/fcm/send/flhAFTILumM:APA91bFSAKnwbgRH0GI9crHAKjQGHViVX9H8wDPbnE8K4ZG54MxijFIx0uOp1nFa-8GNhAP9lmWeFUNkvYFtzgEwVvocTYnxWNPnrLrHXKUxUD6UvLxc6zs6P41NtVKdj0XU9ZQMhElH",
+  endpoint:"https://fcm.googleapis.com/fcm/send/fyQ0ND72_wk:APA91bG62_Yro5jx8x-n5wFbfYDQE2Bek-kN6X2xk2KCRT7t_Mxioldyh4E27fP_hCw2VtxGl6O9pJ6euoQBUZHLhYZZjOJ8DMB6SaDFdj9FIIzUG8i_CbiAobVd6ljH1EpGPUY5V3Ky",
   expirationTime:null,
-  keys:{p256dh:"BNrXZEDihtOERl4Q_uX16LgY7YhxJv4w6NjXo9NqdFH9129rctpNKuMZPcEzxtTnK74Yxjne8jqTMe8ivJdp4gY",
-  auth:"qFMCZjTltn_2mvMyPx-CZA"}}
+  keys:{p256dh:"BGtswv8pnz7Rx3i5h7IbnBrZvMVsmRr7QO12RCKLY8_U5GOrZeBVwFtHpZQaUk89AJAoogcP-T4aISzk-FMGBvo",
+  auth:"_243pMDhD0lhG7xMXxwV7w"}}
 
 push.sendNotification(sub, 'test message')
--- sw.js
+++ sw.js
@@ -15,6 +15,6 @@ self.addEventListener('push', function(e) {
         ]
     };
     e.waitUntil(
-        self.registration.showNotification('Hello world!', options)
+        self.registration.showNotification('순번대기 완료!', options)
     );
 });
